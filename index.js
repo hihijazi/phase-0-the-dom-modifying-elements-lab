26b25fd3ed2519082a25f36d666e06354e34a798
hadil <hihijazi@gmail.com>
2023-11-02 00:03:49 -0400

    save changes

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+const newHeader = document.createElement('h1');
+const yourName = 'Your Name';
+    newHeader.textContent = `${yourName} is the champion`;
+newHeader.id = 'victory';
